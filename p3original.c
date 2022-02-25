fe7fa822a4d2dc8735213b9e17eae926fd7e69ab
Yatham Tejasri <94678025+Sridha123@users.noreply.github.com>
2022-02-25 07:27:31 +0000

    p3final.c

---
+++ p3original.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter the number\n");
+  scanf("%d",&n);
+  return n;
+  }
+int is_composite(int n)
+{
+  int i=2;
+  while(i<=n/2)
+{
+  if(n%i==0)
+  return 1;
+  i++;
+  }
+  return 0;
+  }
+void output(int n,int is_composite)
+{
+  if(is_composite)
+  {
+    printf("%d is a composite number\n" ,n);
+    }
+  else
+  {
+    printf("The number is not composite\n");
+    }
+  }
+int main()
+{
+  int n=input_number();
+  int composite= is_composite(n);
+  output(n,composite);
+  return 0;
+  }
+
